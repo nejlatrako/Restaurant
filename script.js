d8e8d9199ee26bb638537d0a5c72e156a19287b5
TNejla <nejla.trako.20@size.ba>
2022-05-17 22:19:03 +0200

    get and post methods

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('proba')
+
+fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        const html = data.map(food =>{
+            return `
+            <div class="meals">
+                <div class="mealPic">
+                    <img src="${food.imageUrl}" alt="picture">
+                </div>
+                <div class="description">
+                    <h3> Meal Name : ${food.name}</h3>
+                    <p> ID: ${food.id} <br>
+                     Price : ${food.price}</p>
+
+                </div>
+            </div>
+            `;
+        }).join('');
+        console.log(html);
+        document
+            .querySelector('#menu')
+            .insertAdjacentHTML("afterbegin", html);
+    } )
+    
+
+fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food', {
+        method : 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": 0,
+            "name": "string",
+            "price": 0,
+            "imageUrl": "string"
+        })
+    })
+    .then(res => {
+        console.log(res);
+    })
